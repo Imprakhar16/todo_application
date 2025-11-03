147fe33daac6315daf33f1c9cdd7f1d78f55c561
prakhar16 <sharmaprakhar1605@gmail.com>
2025-11-03 15:56:12 -0800

    add cors

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js"
 import  todoRoutes from "./routes/todoRoutes.js"
+import cors from "cors";
+
 const app = express();
 const PORT = 3000;
 
 connectDB()
 app.use(express.json())
+app.use(cors())
+
 
 app.use("/api/auth",authRoutes)
 app.use("/api/todos",todoRoutes)
