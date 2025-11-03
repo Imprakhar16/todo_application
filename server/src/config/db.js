eebb761b120416b4824dcf5618899d79b6d40297
prakhar16 <sharmaprakhar1605@gmail.com>
2025-11-03 12:09:16 -0800

    integrate mongoDB Atlas

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+dotenv.config();
 
 const connectDB = async ()=>{
     try {
-        const conn  = mongoose.connect("mongodb://localhost:27017/demo");
+        const conn  = mongoose.connect(process.env.MONGO_URI);
     } catch (error) {
         console.error(`Error: ${error.message}`);
     }
